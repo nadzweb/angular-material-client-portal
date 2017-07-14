ce85263068003966a49031789c0403bd5aea1ab0
nadzweb <nadeem.lala1@gmail.com>
2017-07-14 17:10:23 +1200

    Minor restructuring changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,7 @@ import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
   
